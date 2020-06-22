54415b2113aeedf320a2d486adbfc418ba30fcba
aramdov <45133554+aramdov@users.noreply.github.com>
2020-06-21 23:50:34 -0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -96,3 +96,70 @@ def kangaroo(x1, v1, x2, v2):
 
     else: # all other cases
         return ("NO")
+
+### between 2  sets,
+    # https://www.hackerrank.com/challenges/between-two-sets/problem
+def getTotalX(a, b):
+
+    # this is my first draft of code, definitely a challenging problem, im starting
+    # to learn the basics of algorithmic complexity and O notation,
+    between_counter = 0
+    last_arr_counter = 0
+    last_arr_counter_brr = 0
+    joint_counter = 0
+
+    # first 3 loops just checks arr[-1] for inclusive set, this code is not
+    # very efficient in my opinion, definitely not generalizable as well
+    for i in range(len(arr)):
+        if arr[-1] % arr[i] == 0: # check last arr against every other arr for %
+            last_arr_counter = last_arr_counter + 1 # counter for conditions
+    
+    for z in range(len(brr)):
+        if brr[z] % arr[-1] == 0:
+            last_arr_counter_brr = last_arr_counter_brr + 1
+  
+    if last_arr_counter == len(arr): 
+        if last_arr_counter_brr == len(brr):
+            between_counter = between_counter + 1
+    
+    # attempted to make this code efficient and generalizable
+    # i create a list of all multiples of elements in arr that are less than
+    # first element in brr so it satisfies the condition to be between 2 sets,
+    arr_multiples_bet = []
+
+    for n in range(len(arr)):
+        for j in range(1, int(brr[0]/arr[n])+1):
+            if (arr[n]*j) > arr[-1] and (arr[n]*j) <= brr[0]:
+                arr_multiples_bet.append(arr[n]*j)
+    
+    arr_multiples_bet = list(set(arr_multiples_bet)) # remove duplicates
+    combined = arr + brr # combine list to make iteration easier
+    # debugger print(arr_multiples_bet)
+    # debugger print(combined)
+
+    # check each multiple to be a factor for both arr and brr list with combined list
+    # make a counter that resets after each outer loop iteration or each multiple
+    # if multiple is a factor for everything(arr+brr), it triggers counter + 1,
+    # if not, it resets before going onto the next multiple to be checked,
+    for k in range(len(arr_multiples_bet)):
+
+        joint_counter = 0
+
+        for l in range(len(combined)):
+
+            if arr_multiples_bet[k] == combined[l]:
+                joint_counter = joint_counter + 1
+
+            if arr_multiples_bet[k] > combined[l]:
+                if arr_multiples_bet[k] % combined[l] == 0:
+                    joint_counter = joint_counter + 1
+            
+            if arr_multiples_bet[k] < combined[l]:
+                if combined[l] % arr_multiples_bet[k] == 0:
+                    joint_counter = joint_counter + 1
+            
+            if joint_counter == len(combined):
+                between_counter = between_counter + 1
+
+    return between_counter
+            
